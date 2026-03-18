c34139ac3f2c77be7233805b5a4f95fe2678577a
Rey1718 <reylenin18@gmail.com>
2026-03-18 17:23:41 -0300

    idk

--- XOR.py
+++ XOR.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
 
 # datos XOR
 X = torch.tensor([
@@ -10,12 +12,7 @@ X = torch.tensor([
     [1,1]
 ], dtype=torch.float32)
 
-y = torch.tensor([
-    [0],
-    [1],
-    [1],
-    [0]
-], dtype=torch.float32)
+y = torch.tensor([[0], [1], [1], [0]]).float()
 
 # modelo
 model = nn.Sequential(
@@ -47,3 +44,38 @@ for epoch in range(100000):
 with torch.no_grad():
     print(model(X))
    
+
+
+
+   # Crear una malla de puntos
+x = np.linspace(0, 1, 100)
+y = np.linspace(0, 1, 100)
+xx, yy = np.meshgrid(x, y)
+
+grid = torch.tensor(np.c_[xx.ravel(), yy.ravel()]).float()
+
+# Predicciones
+with torch.no_grad():
+    preds = model(grid)
+    preds = preds.reshape(xx.shape)
+
+# Dibujar
+plt.contourf(xx, yy, preds, levels=50)
+plt.colorbar()
+
+# Datos originales
+plt.scatter(X[:,0], X[:,1], c=y, edgecolors='k')
+plt.title("Frontera de decisión")
+plt.show()
+
+
+test = torch.tensor([
+    [0.1, 0.1],
+    [0.1, 0.9],
+    [0.9, 0.1],
+    [0.9, 0.9]
+]).float()
+
+with torch.no_grad():
+    hidden = model[0](X)  # primera capa
+    print(hidden)
